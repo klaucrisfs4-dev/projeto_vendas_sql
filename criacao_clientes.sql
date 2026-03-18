e14c3d43795dcf0656aae5a86f06f53fa381620c
klaucrisfs4-dev <klaucrisfs4@gmail.com>
2026-03-18 11:13:06 -0300

    Initialize database and table for clientes
    Create database and table for client management with an additional column for registration date.

--- criacao_clientes.sql
+++ criacao_clientes.sql
@@ -1,1 +1,17 @@
 create database criacao_clientes
+
+use criacao_cleintes
+
+create database criacao_clientes;
+
+CREATE TABLE clientes (
+    id_vendedor INT PRIMARY KEY,
+    nome_vendedor VARCHAR(100),
+    equipe VARCHAR(100)
+);
+
+
+
+ALTER TABLE clientes 
+ADD COLUMN data_cadastro DATE AFTER equipe; 
+-- O 'AFTER' é opcional, serve para posicionar a coluna onde você quiser
